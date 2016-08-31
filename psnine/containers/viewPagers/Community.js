af5268ccc389803d63fee75ce8d4762c07b05f26
Smallpath <smallpath2013@gmail.com>
2016-09-01 01:54:59 +0800

    修复主题文字三行时提示行被覆盖的问题

--- psnine/containers/viewPagers/Community.js
+++ psnine/containers/viewPagers/Community.js
@@ -96,12 +96,12 @@ class Community extends Component {
               <Text
                 ellipsizeMode={'tail'}
                 numberOfLines={3}
-                style={{ flex: 2,color: 'black', }}>
+                style={{ flex: 2.5,color: 'black', }}>
                 {rowData.title}
               </Text>
 
-              <View style={{ flex: 1, flexDirection: 'row', justifyContent :'space-between', }}>
-                <Text style={{ flex: -1, color: idColor,textAlign : 'center', textAlignVertical: 'center' , marginLeft: 5, marginRight: 5}}>{rowData.psnid}</Text>
+              <View style={{ flex: 1.1, flexDirection: 'row', justifyContent :'space-between' }}>
+                <Text style={{ flex: -1, color: idColor,textAlign : 'center', textAlignVertical: 'center' }}>{rowData.psnid}</Text>
                 <Text style={{ flex: -1,textAlign : 'center', textAlignVertical: 'center' }}>{fromNow}</Text>
                 <Text style={{ flex: -1,textAlign : 'center', textAlignVertical: 'center' }}>{rowData.count}回复</Text>
                 <Text style={{ flex: -1,textAlign : 'center', textAlignVertical: 'center' }}>{rowData.views}浏览</Text>
