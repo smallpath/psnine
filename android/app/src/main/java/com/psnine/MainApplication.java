15dbafa236ad43cd64cf26f3f6cbad12777aa644
Smallpath <smallpath2013@gmail.com>
2017-04-30 00:37:06 +0800

    编译矢量图标

--- android/app/src/main/java/com/psnine/MainApplication.java
+++ android/app/src/main/java/com/psnine/MainApplication.java
@@ -2,6 +2,7 @@ package com.psnine;
 
 import android.app.Application;
 
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -22,7 +23,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(), new VectorIconsPackage()
       );
     }
   };
