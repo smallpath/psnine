8a54bb17c9a67a364a414f7cbc577e406333295d
Smallpath <smallpath2013@gmail.com>
2017-05-16 14:14:15 +0800

    减少排行的解析循环次数以加快载入

--- psnine/parser/rank.js
+++ psnine/parser/rank.js
@@ -9,35 +9,23 @@ export default function (html) {
   $('table tr').each(function (i, elem) {
     const $this = $(this)
     const info = {}
-    // console.log($this.text())
-    $this.find('td').each(function (j, elem) {
-      const that = $(this)
-      if (j === 0) {
-        info.rank = that.text()
-      } else if (j === 1) {
-        info.avatar = that.find('img').attr('src')
-        info.href = that.find('a').attr('href')
-        info.psnid = that.find('a').attr('href').split('/').pop()
-      } else if (j === 2) {
-        info.content = `<div>${that.html().trim().replace(/<p>.*?<\/p>/,'')}</div>`
-      } else if (j === 3) {
-        info.exp = that.find('em').text()
-        info.level = that.html().replace(/<em>.*?<\/em>/, '').trim()
-      } else if (j === 4) {
-        info.games = that.text()
-      } else if (j === 5) {
-        info.perfectRate = that.text()
-      } else if (j === 6) {
-        info.platinum = that.text()
-      } else if (j === 7) {
-        info.gold = that.text()
-      } else if (j === 8) {
-        info.silver = that.text()
-      } else if (j === 9) {
-        info.bronze = that.text()
-      }
-    })
+
     const text = $this.text()
+    const arr = text.split('\n').map(item => item.trim()).filter(item => item)
+    info.rank = arr[0]
+    info.avatar = $this.find('img').attr('src')
+    info.href = $this.find('a').attr('href')
+    info.psnid = arr[1]
+    info.exp = (arr[2].split('经验') || [])[1]
+    info.level = (arr[2].split('经验') || [])[0]
+    info.content = `<div>${$this.find('p').parent().html().trim().replace(/<p>.*?<\/p>/,'')}</div>`
+    info.games = arr[3]
+    info.perfectRate =arr[4]
+    info.platinum = arr[5]
+    info.gold = arr[6]
+    info.silver = arr[7]
+    info.bronze = arr[8]
+
     if (text.includes('Z币')) {
       info.type = 'zb'
     } else if (text.includes('N币')) {
