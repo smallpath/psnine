1f10a18f552d40f09e10a1b11c90dc0170841f0a
Smallpath <smallpath2013@gmail.com>
2017-06-26 22:35:28 +0800

    快捷回复页增加文字弹窗

--- psnine/container/new/Toolbar.js
+++ psnine/container/new/Toolbar.js
@@ -8,13 +8,14 @@ import {
   Alert,
   ListView,
   ScrollView,
-  Picker
+  Picker,
+  TextInput
 } from 'react-native';
 
 import { connect } from 'react-redux';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import { standardColor, accentColor } from '../../constants/colorConfig';
-
+import MyDialog from '../../components/Dialog'
 import { pngPrefix, getDealURL, getHappyPlusOneURL, getStoreURL } from '../../dao';
 
 import { safeLogin, registURL } from '../../dao/login';
@@ -30,14 +31,19 @@ let dataSource = new ListView.DataSource({
 const items = [
   {
     text: '加粗',
-    onPress: function() {
+    tip: '要加粗的文字',
+    onPress: function(index) {
       const { params } = this.props.navigation.state
       const { callback } = params
       if (callback) {
-        this.props.navigation.goBack()
-        callback({ 
-          text: '[b][/b]',
-          offset: 3
+        this.setState({
+          modalIndex: index,
+          completeCallback: (text) => {
+            this.props.navigation.goBack()
+            callback({ 
+              text: `[b]${text}[/b]`
+            })
+          }
         })
       }
     }
@@ -45,7 +51,8 @@ const items = [
   {
     text: '彩色字体',
     onPress: null,
-    renderComponent: function() {
+    tip: '要使用彩色字体的文字',
+    renderComponent: function(index) {
       const { modeInfo } = this.props.screenProps
       return (
         <Picker style={{
@@ -63,10 +70,14 @@ const items = [
               color: value
             })
             if (callback) {
-              this.props.navigation.goBack()
-              callback({ 
-                text: `[color=${value}][/color]`,
-                offset: -6
+              this.setState({
+                modalIndex: index,
+                completeCallback: (text) => {
+                  this.props.navigation.goBack()
+                  callback({ 
+                    text: `[color=${value}]${text}[/color]`
+                  })
+                }
               })
             }
           }}>
@@ -82,117 +93,163 @@ const items = [
   },
   {
     text: '居中',
-    onPress: function() {
+    tip: '要居中的文字',
+    onPress: function(index) {
       const { params } = this.props.navigation.state
       const { callback } = params
       if (callback) {
-        this.props.navigation.goBack()
-        callback({ 
-          text: '[align=center]8888[/align]',
-          offset: 3
+        this.setState({
+          modalIndex: index,
+          completeCallback: (text) => {
+            this.props.navigation.goBack()
+            callback({ 
+              text: `[align=center]${text}[/align]`
+            })
+          }
         })
       }
     }
   },
   {
     text: '链接',
-    onPress: function() {
+    tip: '需插入的链接地址',
+    onPress: function(index) {
       const { params } = this.props.navigation.state
       const { callback } = params
       if (callback) {
-        this.props.navigation.goBack()
-        callback({ 
-          text: '[url][/url]',
-          offset: 3
+        this.setState({
+          modalIndex: index,
+          completeCallback: (text) => {
+            this.props.navigation.goBack()
+            callback({ 
+              text: `[url]${text}[/url]`
+            })
+          }
         })
       }
     }
   },
   {
     text: '视频',
-    onPress: function() {
+    tip: '需插入的flash视频链接地址',
+    onPress: function(index) {
       const { params } = this.props.navigation.state
       const { callback } = params
       if (callback) {
-        this.props.navigation.goBack()
-        callback({ 
-          text: '[flash][/flash]',
-          offset: 3
+        this.setState({
+          modalIndex: index,
+          completeCallback: (text) => {
+            this.props.navigation.goBack()
+            callback({ 
+              text: `[flash]${text}[/flash]`
+            })
+          }
         })
       }
     }
   },
   {
     text: '引用',
-    onPress: function() {
+    tip: '需要引用的文字内容',
+    onPress: function(index) {
       const { params } = this.props.navigation.state
       const { callback } = params
       if (callback) {
-        this.props.navigation.goBack()
-        callback({ 
-          text: '[quote][/quote]',
-          offset: 3
+        this.setState({
+          modalIndex: index,
+          completeCallback: (text) => {
+            this.props.navigation.goBack()
+            callback({ 
+              text: `[quote]${text}[/quote]`
+            })
+          }
         })
       }
     }
   },
   {
     text: '图片',
-    onPress: function() {
+    tip: '图片的地址',
+    onPress: function(index) {
       const { params } = this.props.navigation.state
       const { callback } = params
       if (callback) {
-        this.props.navigation.goBack()
-        callback({ 
-          text: '[img][/img]',
-          offset: 3
+        this.setState({
+          modalIndex: index,
+          completeCallback: (text) => {
+            this.props.navigation.goBack()
+            callback({ 
+              text: `[img]${text}[/img]`
+            })
+          }
         })
       }
     }
   },
   {
     text: '刮刮卡',
-    onPress: function() {
+    tip: '需要使用刮刮卡隐藏的文字',
+    onPress: function(index) {
       const { params } = this.props.navigation.state
       const { callback } = params
       if (callback) {
-        this.props.navigation.goBack()
-        callback({ 
-          text: '[mark][/mark]',
-          offset: 3
+        this.setState({
+          modalIndex: index,
+          completeCallback: (text) => {
+            this.props.navigation.goBack()
+            callback({ 
+              text: `[mark]${text}[/mark]`
+            })
+          }
         })
       }
     }
   },
   {
     text: '删除线',
-    onPress: function() {
+    tip: '需应用删除线的文字',
+    onPress: function(index) {
       const { params } = this.props.navigation.state
       const { callback } = params
       if (callback) {
-        this.props.navigation.goBack()
-        callback({ 
-          text: '[s][/s]',
-          offset: 3
+        this.setState({
+          modalIndex: index,
+          completeCallback: (text) => {
+            this.props.navigation.goBack()
+            callback({ 
+              text: `[s]${text}[/s]`
+            })
+          }
         })
       }
     }
   },
   {
     text: '分页',
-    onPress: function() {
+    tip: '分页的标题',
+    onPress: function(index) {
       const { params } = this.props.navigation.state
       const { callback } = params
       if (callback) {
-        this.props.navigation.goBack()
-        callback({ 
-          text: '[title][/title]',
-          offset: 3
+        this.setState({
+          modalIndex: index,
+          completeCallback: (text) => {
+            this.props.navigation.goBack()
+            callback({ 
+              text: `[title]${text}[/title]`
+            })
+          }
         })
       }
     }
   }
 ]
+
+const getMapper = index => {
+  const mapper = items[index].tip
+  if (mapper) return mapper
+  return items[0].tip
+}
 
 class About extends Component {
 
@@ -200,8 +257,19 @@ class About extends Component {
     super(props);
 
     this.state = {
-      color: 'select'
+      color: 'select',
+      text: '',
+      modalIndex: -1,
+      completeCallback: () => {}
     }
+  }
+
+  cb = () => {
+    this.setState({
+      modalIndex: -1,
+    }, () => {
+      this.state.completeCallback && this.state.completeCallback(this.state.text)
+    })
   }
 
   render() {
@@ -220,6 +288,59 @@ class About extends Component {
             this.props.navigation.goBack()
           }}
         />
+        {
+          this.state.modalIndex !== -1 && (
+            <MyDialog modeInfo={modeInfo}
+              modalVisible={this.state.modalIndex !== -1}
+              onDismiss={() => { this.setState({ modalIndex: -1 }); this.isValueChanged = false }}
+              onRequestClose={() => { this.setState({ modalIndex: -1 }); this.isValueChanged = false }}
+              renderContent={() => (
+                <View style={{
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  backgroundColor: modeInfo.backgroundColor,
+                  paddingVertical: 20,
+                  paddingHorizontal: 20,
+                  elevation: 4,
+                  position: 'absolute',
+                  left: 20,
+                  right: 20,
+                  opacity: 1,
+                  flex: 0
+                }} borderRadius={2}>
+                  <Text style={{ alignSelf: 'flex-start', fontSize: 15, color: modeInfo.titleTextColor }}>{'请输入' + getMapper(this.state.modalIndex)}</Text>
+                  <View style={{flex: 1, flexDirection: 'row'}}>
+                    <TextInput placeholder=""
+                      autoCorrect={false}
+                      multiline={false}
+                      autoFocus={true}
+                      numberOfLines={1}
+                      onSubmitEditing={this.cb}
+                      ref={ref => this.text = ref}
+                      onChange={({ nativeEvent }) => { this.setState({ text: nativeEvent.text }) }}
+                      value={this.state.text}
+                      style={[styles.textInput, {
+                        color: modeInfo.titleTextColor,
+                        textAlign: 'left',
+                        flex: 1,
+                        borderBottomColor: modeInfo.brighterLevelOne,
+                        borderBottomWidth: StyleSheet.hairlineWidth
+                      }]}
+                      placeholderTextColor={modeInfo.standardTextColor}
+                      // underlineColorAndroid={accentColor}
+                      underlineColorAndroid={modeInfo.accentColor}
+                    />
+                  </View>
+                  <TouchableNativeFeedback onPress={this.cb}>
+                    <View style={{ alignSelf: 'flex-end', paddingHorizontal: 8, paddingVertical: 5 }}>
+                      <Text style={{color: '#009688'}}>确定</Text>
+                    </View>
+                  </TouchableNativeFeedback>
+                </View>
+              )}>
+            </MyDialog>
+          )
+        }
         <ScrollView style={{ flex: 2, backgroundColor: modeInfo.backgroundColor }}>
           {items.map((item, index) => {
             const inner = (
@@ -231,7 +352,7 @@ class About extends Component {
                 <Text style={[styles.themeName, { flex: 1, color: modeInfo.titleTextColor }]}>
                   {item.text}
                 </Text>
-                {item.renderComponent && item.renderComponent.bind(this)()||undefined}
+                {item.renderComponent && item.renderComponent.bind(this)(index)||undefined}
               </View>
             )
             return item.renderComponent ? (
@@ -239,7 +360,7 @@ class About extends Component {
                 {inner}
               </View>
             ) : (
-              <TouchableNativeFeedback key={index} onPress={item.onPress ? item.onPress.bind(this) : null}>
+              <TouchableNativeFeedback key={index} onPress={item.onPress ? item.onPress.bind(this, index) : null}>
                 {inner}
               </TouchableNativeFeedback>
             )
