e0e7b48f1bdbf4622a7352fbf25f41b51139fc18
Smallpath <smallpath2013@gmail.com>
2017-08-18 23:25:34 +0800

    add some typings

--- psnine/interface/index.d.ts
+++ psnine/interface/index.d.ts
@@ -1,7 +1,27 @@
-// declare global {
-//   namespace NodeJS {
-//     export interface GlobalStatic {
-//       toast(msg: string): void
-//     }
-//   }
-// }
+import {
+  TouchableNativeFeedbackProperties,
+  PickerProperties,
+  StyleProp,
+  ViewStyle as NaniStyle,
+  FlatListProperties
+} from 'react-native'
+
+interface ViewStyles extends NaniStyle {
+  color?: string
+}
+
+declare module 'react-native' {
+  interface TouchableNativeFeedbackProperties {
+    useForeground?: boolean
+  }
+  interface FlatListProperties<ItemT> {
+    windowSize?: number
+    updateCellsBatchingPeriod?: number
+    maxToRenderPerBatch?: number
+    disableVirtualization?: boolean
+    renderScrollComponent?: (props: ScrollViewProperties) => React.ReactElement<ScrollViewProperties>
+  }
+  // interface PickerProperties {
+  //   style?: StyleProp<ViewStyles>
+  // }
+}
